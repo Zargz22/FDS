58d776a1d30cf9c8497eeb1d81b7edf09660c5e4
Zargz22 <enzoguasti@gmail.com>
2024-10-30 13:43:38 -0300

    Update seta.js

--- seta.js
+++ seta.js
@@ -2,19 +2,19 @@ function toggleSection(id, header) {
   const section = document.getElementById(id);
   const arrow = header.querySelector('.arrow');
 
-  // Toggle the visibility of the section
+  // Abilitar a visibilidade das abas
   section.style.display = section.style.display === 'none' ? 'block' : 'none';
 
-  // Toggle the 'rotated' class to rotate the arrow
+  // Para rotacionar a seta
   arrow.classList.toggle('rotated');
 }
 
-// Initialize the visibility of the sections
+// Inicializa a visibilidade das abas
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.plans').forEach(section => {
     section.style.display = 'none';
   });
-  // Set initial arrow state to closed
+  // Definir o estado da seta como fechado
   document.querySelectorAll('.arrow').forEach(arrow => {
     arrow.classList.add('rotated');
   });
